060c1ca582d7132573a04eb300369174b02623c4
Ron Sala <ron.sala@gmail.com>
2018-02-01 08:58:34 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  attr_accessor :name
+  @@song_count = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.song_count
+    @@song_count = @songs.count
+  end
+
+end
