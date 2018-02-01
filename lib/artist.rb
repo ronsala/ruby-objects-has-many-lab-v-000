776b8a20f9586210e1dc782b82315dd10a247aff
Ron Sala <ron.sala@gmail.com>
2018-02-01 09:04:18 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_accessor :name
-  @@song_count = []
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -10,6 +10,7 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def songs
@@ -20,10 +21,11 @@ class Artist
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
-    @@song_count = @songs.count
+    @@song_count
   end
 
 end
