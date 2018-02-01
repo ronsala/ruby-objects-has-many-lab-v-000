26d791798a54958c474d4a7ed9f6125174c201fa
Ron Sala <ron.sala@gmail.com>
2018-02-01 09:12:51 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -17,8 +17,8 @@ class Author
     @posts
   end
 
-  def add_post_by_name(name)
-    post = post.new(name)
+  def add_post_by_title(title)
+    post = Post.new(title)
     @posts << post
     post.author = self
     @@post_count += 1
